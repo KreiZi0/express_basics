bde0d4d8cf8209fc04e387f341e47f16ee3e3251
KreiZi0 <steven.mirontsuk1@gmail.com>
2022-02-22 14:14:48 +0200

    Templating directory setup

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 
 app.use(express.static('public'));
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'ejs');
 
 app.get('/user/:username', (reg, res)=> {
 	let user = reg.params.username;
 
 
-	res.render('index.ejs', {username: user});
+	res.render('index', {username: user});
 });
 
 
