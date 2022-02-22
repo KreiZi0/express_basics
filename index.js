66180bdc0b32cb51fc21736e168b5a6adc8214eb
KreiZi0 <steven.mirontsuk1@gmail.com>
2022-02-22 15:10:06 +0200

    Post request use

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 const express = require('express');
 const app = express();
+
 const path = require('path')
 
-app.use(express.static('public'));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
 
-app.set('view engine', 'ejs');
-
-
-
-app.get('/questions', (reg, res)=> {
-	let questions = [
-		{title: "What is Node.js?", user: "Kadi", votes: "10"},
-		{title: "What is Express.js?", user: "Mike", voes: "8"}
-	]
-
-
-
-	res.render('index', {questions:questions});
+app.get('/login', (req, res)=> {
+	res.render('login');
 });
 
+app.post('/login', (req, res)=> {
+	let username = req.body.username;
+	let password = req.body.password;
 
-app.listen(3000,()=> {
+	if(username === 'user' && password === 'qwerty'){
+		res.redirect('/dashboard');
+	}
+});
+
+app.get('/dashboard', (req, res)=> {
+	res.render('dashboard');
+})
+
+app.listen(3000, ()=> {
 	console.log('Server started on http://localhost:3000');
 });
